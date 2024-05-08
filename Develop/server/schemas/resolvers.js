7b9c498763c16e86923991314a6b959c9ee09d2f
Mark Lewis <mlewis89@gmail.com>
2024-05-09 09:47:59 +1000

    placeholders for resolvers and typedefs

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+//include models
+//const {} = require('../models');
+//const {signToken, AuthencationError} = require('../utils/auth');
+
+const resolvers = {
+    Query : {},
+    Mutation: {},
+}
+
+module.exports = resolvers;
