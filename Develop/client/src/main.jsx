c69adf4cf34bb0704e1ecfb2cf479253fb0ee429
Mark Lewis <mlewis89@gmail.com>
2024-05-11 11:24:47 +1000

    Added in semantic UI

--- Develop/client/src/main.jsx
+++ Develop/client/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 import App from './App.jsx';
 import Home from './pages/Home';
