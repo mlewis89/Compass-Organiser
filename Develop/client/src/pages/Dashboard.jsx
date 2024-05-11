e7924bef9e8a46dafe6af57457336856e45e1936
Mark Lewis <mlewis89@gmail.com>
2024-05-11 17:51:45 +1000

    dashboard tidy

--- Develop/client/src/pages/Dashboard.jsx
+++ Develop/client/src/pages/Dashboard.jsx
@@ -1,38 +1,38 @@
-import { Grid, GridColumn, GridRow, Segment, Label } from "semantic-ui-react"
+import { Grid, GridColumn, GridRow, Segment, Label } from "semantic-ui-react";
 import BoardPostList from "../components/BoardPostList";
 import EventList from "../components/EventList";
 import MyTasks from "../components/MyTasks";
 import MyStats from "../components/MyStats";
 
 const Dashboard = () => {
-  
-
   return (
-    <Grid columns={2}>
-    <GridRow stretched>
-      <GridColumn>
-        <Segment padded>
-          <Label attached='top'>Notice Board</Label>
-          <BoardPostList />
-        </Segment>
-      </GridColumn>
-      <GridColumn>
-      <Segment padded>
-          <Label attached='top'>Upcoming Events</Label>
-          <EventList />
-        </Segment><Segment padded>
-          <Label attached='top'>My Tasks</Label>
-          <MyTasks />
-        </Segment>
-      </GridColumn>
+    <Grid>
+      <GridRow stretched columns={2}>
+        <GridColumn>
+          <Segment padded>
+            <Label attached="top">Notice Board</Label>
+            <BoardPostList />
+          </Segment>
+        </GridColumn>
+        <GridColumn>
+          <Segment padded>
+            <Label attached="top">Upcoming Events</Label>
+            <EventList />
+          </Segment>
+          <Segment padded>
+            <Label attached="top">My Tasks</Label>
+            <MyTasks />
+          </Segment>
+        </GridColumn>
       </GridRow>
-      <GridRow stretched>
-      <GridColumn>
-      <Label attached='top'>My Stats</Label>
+      <GridRow stretched columns={1}>
+        <GridColumn>
+          <Label attached="top">My Stats</Label>
           <MyStats />
         </GridColumn>
-        </GridRow>
-      </Grid>);
+      </GridRow>
+    </Grid>
+  );
 };
 
 export default Dashboard;
