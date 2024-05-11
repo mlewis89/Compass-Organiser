125acd9bf483b1d9c7633bedcb52ce5cc0636024
Mark Lewis <mlewis89@gmail.com>
2024-05-11 17:42:28 +1000

    Event page layout

---
+++ Develop/client/src/components/DetailedEventItem.jsx
@@ -0,0 +1,5 @@
+const DetailedEventItem = () => {
+    return (<div></div>);
+}
+
+export default DetailedEventItem;
--- Develop/client/src/pages/Events.jsx
+++ Develop/client/src/pages/Events.jsx
@@ -1,11 +1,39 @@
+import { Grid, GridColumn, GridRow, Segment, Label } from "semantic-ui-react";
+import EventList from '../components/EventList'
+import DetailedEventItem from '../components/DetailedEventItem'
 
 
-const Events = () => {
+const Events= () => {
+  const myEvents = [];
+  const otherEvents = [];
+  const activeEvent = {};
+  
   return (
-    <div >
-      Events
-      </div>  );
+    <Grid columns={2}>
+      <GridRow columns={2} stretched>
+        <GridColumn>
+          <Segment padded>
+            <Label attached="top">My Events</Label>
+            <EventList  events={myEvents}/>
+          </Segment>
+        </GridColumn>
+        <GridColumn>
+          <Segment padded>
+            <Label attached="top">Other Events</Label>
+            <EventList events={otherEvents} />
+          </Segment>
+        </GridColumn>
+      </GridRow>
+      <GridRow stretched columns={1}>
+        <GridColumn >
+          <Segment padded>
+            <Label attached="top">Event Details</Label>
+            <DetailedEventItem event={activeEvent} />
+          </Segment>
+        </GridColumn>
+      </GridRow>
+    </Grid>
+  );
 };
-
 
 export default Events;
