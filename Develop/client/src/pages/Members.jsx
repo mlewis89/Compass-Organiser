1a1ae042afce49353a3ab7877d3612f0d44f5c31
Mark Lewis <mlewis89@gmail.com>
2024-05-11 17:50:25 +1000

    member placholder

--- Develop/client/src/pages/Members.jsx
+++ Develop/client/src/pages/Members.jsx
@@ -10,58 +10,135 @@ import {
   Label,
   Menu,
   Table,
-} from 'semantic-ui-react'
+} from "semantic-ui-react";
 
 const Members = () => (
   <Table celled>
-  <TableHeader>
-    <TableRow>
-      <TableHeaderCell>Header</TableHeaderCell>
-      <TableHeaderCell>Header</TableHeaderCell>
-      <TableHeaderCell>Header</TableHeaderCell>
-    </TableRow>
-  </TableHeader>
+    <TableHeader>
+      <TableRow>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+        <TableHeaderCell>Header</TableHeaderCell>
+      </TableRow>
+    </TableHeader>
 
-  <TableBody>
-    <TableRow>
-      <TableCell>
-        <Label ribbon>First</Label>
-      </TableCell>
+    <TableBody>
+      <TableRow>
+        <TableCell>
+          <Label ribbon>First</Label>
+        </TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+      </TableRow>
+      <TableRow>
       <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+      </TableRow>
+      <TableRow>
       <TableCell>Cell</TableCell>
-    </TableRow>
-    <TableRow>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+      </TableRow>
+      <TableRow>
       <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+      </TableRow>
+      <TableRow>
       <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+      </TableRow>
+      <TableRow>
       <TableCell>Cell</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell>Cell</TableCell>
-      <TableCell>Cell</TableCell>
-      <TableCell>Cell</TableCell>
-    </TableRow>
-  </TableBody>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+        <TableCell>Cell</TableCell>
+      </TableRow>
+    </TableBody>
 
-  <TableFooter>
-    <TableRow>
-      <TableHeaderCell colSpan='3'>
-        <Menu floated='right' pagination>
-          <MenuItem as='a' icon>
-            <Icon name='chevron left' />
-          </MenuItem>
-          <MenuItem as='a'>1</MenuItem>
-          <MenuItem as='a'>2</MenuItem>
-          <MenuItem as='a'>3</MenuItem>
-          <MenuItem as='a'>4</MenuItem>
-          <MenuItem as='a' icon>
-            <Icon name='chevron right' />
-          </MenuItem>
-        </Menu>
-      </TableHeaderCell>
-    </TableRow>
-  </TableFooter>
-</Table>
+    <TableFooter>
+      <TableRow>
+        <TableHeaderCell colSpan="12">
+          <Menu floated="right" pagination>
+            <MenuItem as="a" icon>
+              <Icon name="chevron left" />
+            </MenuItem>
+            <MenuItem as="a">1</MenuItem>
+            <MenuItem as="a">2</MenuItem>
+            <MenuItem as="a">3</MenuItem>
+            <MenuItem as="a">4</MenuItem>
+            <MenuItem as="a" icon>
+              <Icon name="chevron right" />
+            </MenuItem>
+          </Menu>
+        </TableHeaderCell>
+      </TableRow>
+    </TableFooter>
+  </Table>
 );
 
 export default Members;
-  
