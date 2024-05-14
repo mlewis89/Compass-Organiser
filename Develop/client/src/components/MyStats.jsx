7d9bcb96e94d2a78b7107c1c1604c02cce1a9a80
Mark Lewis <mlewis89@gmail.com>
2024-05-14 22:56:06 +1000

    stats placholder

--- Develop/client/src/components/MyStats.jsx
+++ Develop/client/src/components/MyStats.jsx
@@ -1,5 +1,30 @@
+import {
+  Statistic,
+  StatisticValue,
+  StatisticLabel,
+  Segment,
+  StatisticGroup,
+} from "semantic-ui-react";
 
-const MyStats = () => {return (<></>);
-}
+const MyStats = () => {
+  return (
+    <Segment>
+      <StatisticGroup>
+        <Statistic>
+          <StatisticValue>50</StatisticValue>
+          <StatisticLabel>members</StatisticLabel>
+        </Statistic>
+        <Statistic>
+          <StatisticValue>50</StatisticValue>
+          <StatisticLabel>Tasks</StatisticLabel>
+        </Statistic>
+        <Statistic>
+          <StatisticValue>50</StatisticValue>
+          <StatisticLabel>Events</StatisticLabel>
+        </Statistic>
+      </StatisticGroup>
+    </Segment>
+  );
+};
 
 export default MyStats;
