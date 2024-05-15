05b7149ec3890a0d44a55cf72f6cfec74eec6ba9
Mark Lewis <mlewis89@gmail.com>
2024-05-15 21:54:51 +1000

    added button to suggested tasks to add task

--- Develop/client/src/components/SuggestedTasks.jsx
+++ Develop/client/src/components/SuggestedTasks.jsx
@@ -1,61 +1,77 @@
 import {
-    Table,
-    TableHeader,
-    TableHeaderCell,
-    TableBody,
-    TableRow,
-    TableCell,
-  } from "semantic-ui-react";
-  import { useQuery } from "@apollo/client";
-  import { QUERY_SUGGESTED_TASKS } from "../utils/queries";
-  
-  const SuggestedTasks = () => {
-    const { data } = useQuery(QUERY_SUGGESTED_TASKS);
-    let tasks;
-  
-    let TableHeaderArr = [
-      "name",
-      "description",
-      "priority",
-      "dueDate",
-      "duration",
-   //   "requiredSkills",
-   //   "responsible",
-      "status",
-    ];
-    if (data) {
-      tasks = data.suggestedTasks;
-      console.log(tasks);
-  
-    }
-  
-    return (
-      <Table celled selectable>
-        <TableHeader>
-          <TableRow>
-            {TableHeaderArr.map((header) => (
-              <TableHeaderCell key={header}>{header}</TableHeaderCell>
-            ))}
-          </TableRow>
-        </TableHeader>
-        {tasks ? (
-          <TableBody>
-            {tasks.map((task) => (
-              <TableRow key={task._id}>
-                {TableHeaderArr.map((propertyName) => (
-                  <TableCell key={task._id + propertyName}>
-                    {task[propertyName]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        ) : (
-          <></>
-        )}
-      </Table>
-    );
+  Table,
+  TableHeader,
+  TableHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+} from "semantic-ui-react";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_SUGGESTED_TASKS } from "../utils/queries";
+import { ADD_USER_TASK } from "../utils/mutations";
+
+const SuggestedTasks = () => {
+  const { data } = useQuery(QUERY_SUGGESTED_TASKS);
+  const [addUserTask, { error }] = useMutation(ADD_USER_TASK);
+
+  const handleAddTask = (event, data) => {
+    let _id = data["data-key"];
+    addUserTask({ variables: { taskId: _id } });
   };
-  
-  export default SuggestedTasks;
-  
+
+  let tasks;
+
+  let TableHeaderArr = [
+    "name",
+    "description",
+    "priority",
+    "dueDate",
+    "duration",
+    //   "requiredSkills",
+    //   "responsible",
+    "status",
+  ];
+  if (data) {
+    tasks = data.suggestedTasks;
+    console.log(tasks);
+  }
+
+  return (
+    <Table celled selectable>
+      <TableHeader>
+        <TableRow>
+          {TableHeaderArr.map((header) => (
+            <TableHeaderCell key={header}>{header}</TableHeaderCell>
+          ))}
+        </TableRow>
+      </TableHeader>
+      {tasks ? (
+        <TableBody>
+          {tasks.map((task) => (
+            <TableRow key={task._id}>
+              {TableHeaderArr.map((propertyName) => (
+                <TableCell key={task._id + propertyName}>
+                  {task[propertyName]}
+                </TableCell>
+              ))}
+              <TableCell>
+                <Button
+                  data-key={task._id}
+                  key={task._id}
+                  onClick={handleAddTask}
+                >
+                  Add Task
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      ) : (
+        <></>
+      )}
+    </Table>
+  );
+};
+
+export default SuggestedTasks;
